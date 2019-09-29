aa9953a982db893503f69e481bdcaf3c373cee2f
jennifer <jenniferlrx2@gmail.com>
2019-09-29 10:33:01 -0400

    finished lab 3

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -13,6 +15,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ProfileActivity extends AppCompatActivity {
     ImageButton mImageButton;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +30,51 @@ public class ProfileActivity extends AppCompatActivity {
         EditText email_edit = findViewById(R.id.profile_email);
         email_edit.setText(email);
 
-        ImageButton mImageButton = findViewById(R.id.profile_img);
+        mImageButton = findViewById(R.id.profile_img);
 
         if(mImageButton != null)
             mImageButton.setOnClickListener(v -> {
-                Intent goProfile = new Intent(ProfileActivity.this, ProfileActivity.class);
 
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
 
             });
-
+        Log.e(ACTIVITY_NAME, "In function onCreate" );
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function onStart" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function onResume" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function onPause" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function onStop" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function onDestroy" );
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -43,6 +83,7 @@ public class ProfileActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageButton.setImageBitmap(imageBitmap);
         }
+        Log.e(ACTIVITY_NAME, "In function onActivityResult" );
     }
 
 }
